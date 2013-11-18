364d95ca01d77bb02335a78ab62031bd975d7c42
dingout <dingo.ut@gmail.com>
2013-11-18 09:31:40 -0700

    finished folder and file browsing
    Still need to create a list to pass to ReplayGain method.  Also need M3U
    list to populate the datagrid.

--- ReplayGain/ReplayGain/MainWindow.xaml.cs
+++ ReplayGain/ReplayGain/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * Richard Anderson
+ * CS 2530
+ * Final Team Project
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,11 +23,10 @@ using Microsoft.Win32;
 
 namespace ReplayGain
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> filePaths = new List<string>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -29,12 +34,27 @@ namespace ReplayGain
 
         private void SelectFolderButton_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog dialog = new OpenFileDialog();
             System.Windows.Forms.FolderBrowserDialog folderDialog = new System.Windows.Forms.FolderBrowserDialog();
             folderDialog.RootFolder = Environment.SpecialFolder.Desktop;
             folderDialog.ShowDialog();
             string curDirectory = folderDialog.SelectedPath;
-            FileList.ItemsSource = new DirectoryInfo(curDirectory).GetFiles("*.mp3",SearchOption.AllDirectories);
+            try
+            { 
+                var files = Directory.EnumerateFiles(curDirectory, "*.mp3",SearchOption.AllDirectories);
+                FileList.ItemsSource = files;
+                foreach (var f in files)
+                {
+                    filePaths.Add(f.ToString());
+                }
+            }
+            catch (UnauthorizedAccessException UAEx)
+            {
+                Console.WriteLine(UAEx.Message);
+            }
+            catch (PathTooLongException PathEx)
+            {
+                Console.WriteLine(PathEx.Message);
+            }
         }
 
         private void ClearButton_Click(object sender, RoutedEventArgs e)
@@ -44,7 +64,39 @@ namespace ReplayGain
 
         private void ReplayGainButton_Click(object sender, RoutedEventArgs e)
         {
+            var itemsSource = FileList.ItemsSource as IEnumerable<string>;
+            if (FileList.ItemsSource != null)
+            {
+                foreach (var item in FileList.ItemsSource)
+                {
+                    var row = FileList.ItemContainerGenerator.ContainerFromItem(item) as DataGridRow;
+                    if (row != null) 
+                    {
+                        MessageBox.Show(row.Item.ToString());
+                    }
+                }
+            }
+        }
 
+        private void Button_Settings_Click(object sender, RoutedEventArgs e)
+        {
+            Settings settings = new Settings();
+            settings.Show();
+        }
+
+        private void AddPlaylistButton_Click(object sender, RoutedEventArgs e)
+        {
+            //use openfiledialog to select an m3u file and pass path to Kurts class/method.
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == true) // user clicked OK button
+            {
+                string filePath = dialog.FileName;
+            } 
+        }
+
+        private void Button_Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
